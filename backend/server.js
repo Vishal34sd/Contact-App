1abc3fb87de64e2ff148e2af60eeb0c98f3e24c8
Vishal <dubeyvishal714@gmail.com>
2026-01-03 12:09:37 +0530

    initialize project

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express" ;
+import dotenv from "dotenv" ;
+dotenv.config();
+
+const PORT = process.env.PORT ;
+
+const app = express() ;
+
+
+
+
+
+
+app.listen(PORT , ()=>{
+    console.log(`Server is running on  ${PORT}`);
+})
